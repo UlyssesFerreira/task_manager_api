4bf6e194820056c6dc72e0785322a8526da1bd72
ulysses <ulyssesh.20@gmail.com>
2025-05-27 22:59:47 -0300

    authorize before action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::API
+  before_action :authorize_request
+
+  private
+
+  def authorize_request
+    header = request.headers["Authorization"]
+    token = header.split(" ").last
+    begin
+      decoded = JWT.decode(token, Rails.application.credentials.secret_key_base)[0]
+      @current_user = User.find(decoded["data"])
+    rescue JWT::ExpiredSignature
+      render json: { error: "Expired token" }, status: :unauthorized
+    rescue JWT::DecodeError
+      render json: { error: "Invalid token" }, status: :unauthorized
+    end
+  end
 end
--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,8 +1,10 @@
 class AuthController < ApplicationController
+  skip_before_action :authorize_request
+
   def login
     user = User.find_by(email: params[:email])
     if user.authenticate(params[:password])
-      expiration = Time.now.to_i + 120
+      expiration = Time.now.to_i + 60
       payload = {
         data: user.id,
         exp: expiration
@@ -10,7 +12,7 @@ class AuthController < ApplicationController
       token = JWT.encode(payload, Rails.application.credentials.secret_key_base)
       render json: { access_token: token, expires_at: Time.at(expiration) }
     else
-      render json: { error: "Invald credentials" }, status: :unauthorized
+      render json: { error: "Invalid credentials" }, status: :unauthorized
     end
   end
 end
